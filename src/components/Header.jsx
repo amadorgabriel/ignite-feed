c7ac4fafabb4db09dd82b801e05fc58422d14647
Gabriel Rodrigues Amador <gabriel.amador@sp.senai.br>
2023-01-09 17:44:57 -0300

    feat: add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import styles from './styles.module.css'
+import styles from './header.module.css'
+
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Ignite Feed</h1>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   )
 }
